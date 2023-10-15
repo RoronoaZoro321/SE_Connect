aaeeca4f69f41d767d2faeee95d79d098c155394
SSYino <gianmario.nino@gmail.com>
2023-10-16 00:18:27 +0700

    Run backend/main

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import uvicorn
-from backend.main import app  # Assuming your FastAPI app is defined in backend/main.py
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
     # run ipconfig in cmd
     # find IPv4 Address
     # enter in the url bar of your browser: http://IPv4:8000
     # now can be access from any device in the same network
-
-
