3208f2d9de09983b0bfe35aa6214406b62b8ae8d
lolipopfounder <87083962+lolipopfounder@users.noreply.github.com>
2023-10-16 15:29:41 +0700

    update main

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,12 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from backend.core.config import settings
 from backend.apis.base import api_router
-from backend.db.models import User, Post
+from backend.db.models import User
+import BTrees.OOBTree
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 app.include_router(api_router)
@@ -14,46 +19,40 @@ app.include_router(api_router)
 templates = Jinja2Templates(directory="backend/templates")
 
 # Create a ZODB storage and database connection
-storage = ZODB.FileStorage.FileStorage("backend/db/mydb.fs")
+storage = ZODB.FileStorage.FileStorage("backend/db/db.fs")
 db = ZODB.DB(storage)
 connection = db.open()
 root = connection.root()
-if "users" not in root:
-    root["users"] = {}
+
+# Use BTrees.OOBTree to store users
+root.users = BTrees.OOBTree.BTree()
 
 # Home route
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-
 # Login route
 @app.post("/login")
 async def login(username: str = Form(...), password: str = Form(...)):
-    user = root["users"].get(username)
+    user = root.users.get(username)
     if user is None or user.password != password:
         raise HTTPException(status_code=400, detail="Invalid username or password")
     return {"message": "Login successful"}
 
 @app.post("/signup")
-async def signup(id: int, username: str, firstName: str, lastname: str, password: str):
+def signup(id: int, username: str, firstName: str, lastname: str, password: str):
     try:
         # Check for duplicate id
-        if id in root["users"]:
+        if id in root.users:
             raise HTTPException(status_code=400, detail="This Id already exists")
-        print("here1")
-        # Create a UserData object and store it in the ZODB
+        # Create a UserData object and store it in the BTrees.OOBTree
         user = User(id, username, firstName, lastname, password)
-        root["users"][id] = user
-        print("here2")
-
+        root.users[id] = user
 
         # Commit the transaction
         transaction.commit()
-        print("here3")
 
-
-        # return templates.TemplateResponse("signup_success.html", {"request": request})
         return {"message": "User created successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -62,18 +61,21 @@ async def signup(id: int, username: str, firstName: str, lastname: str, password
 @app.get("/getUser")
 def getUserSingle(user_id: int):
     try:
-        user = root["users"].get(user_id)
+        users = root.users
+        if users is None:
+            raise HTTPException(status_code=400, detail="No users found")
+        user = users.get(user_id)
         if user is None:
-            raise HTTPException(status_code=400, detail="User not found")
+            raise HTTPException(status_code=400, detail="No user found for this id")
         return user
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 # get all users
-@app.get("/getUsers")
-def getUsers():
+@app.get("/getAllUsers")
+def getAllUsers():
     try:
-        users = root["users"]
+        users = root.users
         if users is None:
             raise HTTPException(status_code=400, detail="No users found")
         return users
@@ -83,4 +85,5 @@ def getUsers():
 # Close the database connection when the application stops
 @app.on_event("shutdown")
 async def shutdown():
+    transaction.commit()
     connection.close()
